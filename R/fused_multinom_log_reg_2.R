cc103fa9c80027e3ed39a4d40e078830ae5a7c93
jaredhuling <huling@wisc.edu>
2014-04-27 20:17:49 -0500

    typo

--- R/fused_multinom_log_reg_2.R
+++ R/fused_multinom_log_reg_2.R
@@ -444,7 +444,7 @@ fusedMultinomialLogistic2 <- function(x, y, lambda, groups = NULL,
         #l.sum <- fun.beta - fun.s - (as.double(sum(diag(crossprod(v, g))))) - sum((c - sc) * gc)
         
         #l.sum <- fun.beta - fun.s; r.sum <- 1e-10
-        cat("r.sum: ", r.sum, "l.sum: ", l.sum, "fun.beta: ", fun.beta, "fun.s: ", fun.s, " L: ", L)
+        #cat("r.sum: ", r.sum, "l.sum: ", l.sum, "fun.beta: ", fun.beta, "fun.s: ", fun.s, " L: ", L)
         
         if (r.sum <= 1e-3) {
           #this shows that the gradient step makes little improvement
